bf209e256a2023af315179d5185cb014aeaf0be5
lorencsejdaras <43918041+lorencsejdaras@users.noreply.github.com>
2020-09-08 09:31:01 +0200

    login page

--- login.php
+++ login.php
@@ -1,11 +1,107 @@
 
+
+<?php
+
+require_once "includes/db.php";
+/*
+
+echo "<pre>";
+var_dump($_POST);
+echo "<pre>";
+
+*/
+
+$email_err = $password_err = '';
+$email = $password = '';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+  if(empty(trim($_POST['email']))){
+
+      $email_err = "*E-mail can't be empty.";
+
+      }
+      else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            $email_err = "*Please enter email in the correct format.";
+            }
+            else{
+                    $email = trim($_POST['email']);
+            }
+
+
+
+
+
+
+      if(empty(trim($_POST['password']))){
+          $password_err = 'Please enter a password.';
+        }else { $password = trim($_POST['password']);}
+
+
+        if(empty($email_err) && empty($password_err)){
+
+          $sql = 'SELECT id, email, password FROM users WHERE email = ?';
+
+          if($stmt = $conn->prepare($sql)){
+
+            $stmt->bind_param('s', $param_email);
+
+          $param_email = $email;
+
+            if($stmt->execute()){
+
+              $stmt->store_result();
+
+              if($stmt->num_rows == 1){
+$stmt->bind_result($id, $email, $hashed_password);
+
+if($stmt->fetch()){
+
+  if(password_verify($password, $hashed_password)){
+
+    session_start();
+  echo "logged in";
+
+  $_SESSION['logged_in'] = true;
+  $_SESSION['email'] = $email;
+
+  header('location: home.php');
+
+  }else
+
+  die('the password was not correct');
+}
+
+              }
+            }
+
+
+
+          }
+
+        }
+
+
+
+
+
+
+}
+ ?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <meta charset="utf-8">
-    <title>Sign up</title>
+    <title>Login</title>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
@@ -17,40 +113,8 @@
   </style>
 
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
-<img src="images/foto1.png" alt="foto" style="max-width: 35px;">
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <?php require_once('includes/nav_bar.php'); ?>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-
-      </div>
-    </nav>
 <div class="container my-4">
 
 <h2> Log in</h2>
@@ -65,13 +129,13 @@
     <div class="form-group">
       <label for="email">E-mail</label>
       <input class="form-control" type="text" name="email" id="email" value="" placeholder="your email..." required>
-    <span class="error"> <?php /*echo $email_err;*/ ?></span>
+    <span class="error"> <?php echo $email_err; ?></span>
     </div>
 
     <div class="form-group">
       <label for="password">Password</label>
       <input class="form-control"  type="password" name="password" id="password" value="" placeholder="your password..." required>
-    <span class="error"> </span>
+    <span class="error"><?php echo $password_err; ?> </span>
     </div>
 
     <div class="form-group">
@@ -95,7 +159,6 @@
 
 
 </div>
-
 
   </body>
 </html>
